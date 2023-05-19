36c8e7360725318911517eefb4b8925fe732cc3e
L Zou <16361378+zouluyang@users.noreply.github.com>
2023-05-19 14:39:33 +0800

    finish toggle view in script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
 let childElements = document.getElementById('nav').children;
+let mainElement = document.getElementsByTagName('main')[0];
+let headerElement = document.getElementsByTagName('header')[0];
 
 const toggle = (event) => {
     let id = event.target.innerText.toLowerCase();
+    let targetElement = document.getElementsByClassName(id)[0];
+    let exceptionElements = [];
 
-    console.log(document.getElementsByClassName(id)[0].style.display);
+    for (const element of childElements) {
+        if (element.id !== id) {
+            exceptionElements.push(element);
+        }
+    }
 
-    switch (document.getElementsByClassName(id)[0].style.display) {
-        case 'none':
-            document.getElementsByClassName(id)[0].style.display = 'block';
-            document.getElementsByTagName('main')[0].style.flex = '1';
-            document.getElementsByTagName('header')[0].style.flex = '0';
+    switch (targetElement.hidden) {
+        case false:
+            if (mainElement.hasAttribute('hidden')) {
+                mainElement.removeAttribute('hidden');
+                targetElement.removeAttribute('hidden');
+                mainElement.style.flex = '1';
+                headerElement.style.flex = '0';
+            } else {
+                targetElement.setAttribute('hidden', '');
+                mainElement.style.flex = '0';
+                headerElement.style.flex = '1';
+            }
             break;
-        case 'block':
-            document.getElementsByClassName(id)[0].style.display = 'none';
-            document.getElementsByTagName('main')[0].style.flex = '0';
-            document.getElementsByTagName('header')[0].style.flex = '1';
+        case true:
+            targetElement.removeAttribute('hidden');
+            mainElement.removeAttribute('hidden');
+            for (const element of exceptionElements) {
+                if (element.id !== id) {
+                    document.getElementsByClassName(element.id)[0].setAttribute('hidden', '');
+                }
+            }
+            mainElement.style.flex = '1';
+            headerElement.style.flex = '0';
             break;
         default:
-            document.getElementsByClassName(id)[0].style.display = 'block';
-            document.getElementsByTagName('main')[0].style.flex = '1';
-            document.getElementsByTagName('header')[0].style.flex = '0';
+            console.log('invalid');
             break;
     }
 }
@@ -27,3 +46,11 @@ const toggle = (event) => {
 for (let i = 0; i < childElements.length; i++) {
     childElements[i].addEventListener('click', toggle);
 }
+
+const returnHomePage = () => {
+    mainElement.style.flex = '0';
+    headerElement.style.flex = '1';
+    mainElement.setAttribute('hidden', '');
+}
+
+document.getElementsByTagName('h1')[0].onclick = returnHomePage;
