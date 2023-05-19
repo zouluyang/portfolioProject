1deea3c85a0b699f1b5dff7a52efab7d242445e5
L Zou <16361378+zouluyang@users.noreply.github.com>
2023-05-19 11:04:35 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* let aboutNav = document.getElementsByClassName('about')[0];
+let aboutSection = document.getElementById('about');
+
+const displaySection = (section) => {
+    section.style.display = 'block';
+}
+
+aboutNav.onclick = () => displaySection(aboutSection);
+*/
+
+
